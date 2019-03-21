9745c70dd66402c567b34e71c6f229b257c24c2c
Robin Jongen <robin_jongen@live.nl>
2019-03-21 15:27:32 +0100

    update POC 3

---
+++ 3. Backend information/Assets/Scripts/cubeInteraction.cs
@@ -0,0 +1,56 @@
+﻿using HoloToolkit.Unity.InputModule;
+using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class cubeInteraction : MonoBehaviour, IFocusable, IInputClickHandler
+{
+    public TextMesh feedback;
+    
+    // Use this for initialization
+    void Start()
+    {
+        feedback.text = "Started";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnFocusEnter()
+    {
+    }
+
+    public void OnFocusExit()
+    {
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        feedback.text = "Tapped on object.";
+
+        string url = @"https://pokeapi.co/api/v2/pokemon/6/";
+
+        StartCoroutine(GetCall(url));
+
+    }
+    IEnumerator GetCall(string url)
+    {
+        feedback.text = "Getting from API";
+        UnityWebRequest uwr = UnityWebRequest.Get(url);
+        yield return uwr.SendWebRequest();
+        if (uwr.isNetworkError)
+        {
+            Debug.Log("Error While Sending: " + uwr.error);
+        }
+        
+        var parsedObject = JObject.Parse(uwr.downloadHandler.text);
+        
+        feedback.text = parsedObject["name"].ToString();
+    }
+}
--- 3. Backend information/README.md
+++ 3. Backend information/README.md
@@ -1,3 +1,8 @@
-# qlabs hololens project
+# Backend information
 
-TODO
+This unity solution contains a cube, textblock and a script. These trigger eachother to act when some events happen.
+
+When the user taps the cube the script will do a http get call. This will be from the pokemon api where it will get the 6th pokemon, named charizard. This name will be displayed once the call succeeds.
+
+To build the project click in the top left corner on File/Open scene. In this menu open the Main scene.
+After opening the scene click in the top left corner on File/Build settings... Switch the platform to Universal Windows Platform with the target device set to HoloLens. Then build the solution in the desired folder (usually /App). After building the file explorer will show, click on the build folder (usually /App) and run the .sln with visual studio 2019. When launched you can deploy the application to the HoloLens using Release, x86 and the desired way of deploying.
