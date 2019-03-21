9a0d1ccf9dc2db0d3d2826ad371d227a11ff2d24
Robin Jongen <robin_jongen@live.nl>
2019-03-21 13:04:19 +0100

    add second project, unity project

---
+++ 2. Object and script binding/Assets/Script/cubeInteraction.cs
@@ -0,0 +1,45 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeInteraction : MonoBehaviour, IFocusable, IInputClickHandler
+{
+    public float Speed;
+    public Vector3 ScaleChange;
+
+    public TextMesh feedback;
+
+    private bool Rotating;
+
+    // Use this for initialization
+    void Start()
+    {
+        feedback.text = "Started";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Rotating)
+            transform.Rotate(Vector3.up * Time.deltaTime * Speed);
+    }
+
+    public void OnFocusEnter()
+    {
+        feedback.text = "Focus on object.";
+        Rotating = true;
+    }
+
+    public void OnFocusExit()
+    {
+        feedback.text = "Focus off object.";
+        Rotating = false;
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        feedback.text = "Tapped on object.";
+        transform.localScale += ScaleChange;
+    }
+}
--- 2. Object and script binding/README.md
+++ 2. Object and script binding/README.md
@@ -1,3 +1,11 @@
-# qlabs hololens project
+# Object and script binding
 
-TODO
+This unity solution contains a cube, textblock and a script. These trigger eachother to act when some events happen.
+
+When watching at the cube the text will show "Focus on cube."
+When not watching at the cube the text will show "Focus off cube."
+When the cube is tapped the text will show "Cube tapped."
+
+These texts come from the cubeInteraction.cs script in the Assets/script folder. The script is bound on the cube, together with the gaze and tapmanager it will be triggered.
+
+To build the project click in the top left corner on File/Build settings... then click Build. After completion the file explorer will show, click on the App folder and run the .sln with visual studio 2019. When launched you can deploy the application to the HoloLens.
